aa71fd2085f6f3a2ea797b33e74b2f074c0fd0db
wikk-chy <19112692639@163.com>
2024-06-14 00:18:43 +0800

    delet no use parameter

--- celltypedefined/typingcells.py
+++ celltypedefined/typingcells.py
@@ -1,17 +1,5 @@
-from .utils import define_celltype, deal_celltype_dict
+from .utils import define_celltype
 import pandas as pd
-
-###cell_dict
-# cell_types = {
-#     'B_cells': ['Dennd5b', 'Pax5'],
-#     'T_cells': ['Cd28'],
-#     'NK_cells': ['Il2rb', 'Cd7'],
-#     'Neutrophils': ['G0s2'],
-#     'Macrophages': ['Il1r2'],
-#     'Fibroblasts': ['Col1a1', 'Apoa1'],
-#     'Epithelial_cells': ['Sftpc'],
-#     'Endothelial_cells': ['Npr3', 'Vwf']
-# }
 
 def typing_matrix(input_path, cell_types, 
                   footprint=[0.7, 0.3], times=1.5, threshold=3):
